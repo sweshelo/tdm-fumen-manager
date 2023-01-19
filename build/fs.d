f75df523ae48b898032db15f53d8f754a78e8213
sweshelo <sweshelo@gmail.com>
2023-01-19 18:39:28 +0900

    [delete] build/

--- build/fs.d
+++
@@ -1,3 +0,0 @@
-fs.o: C:/Users/sweshelo/devkitPro/tdm-fumen-manager/source/fs.cpp \
- C:/Users/sweshelo/devkitPro/tdm-fumen-manager/source/include/fs.hpp
-C:/Users/sweshelo/devkitPro/tdm-fumen-manager/source/include/fs.hpp:
--- build/songs.d
+++
@@ -1,7 +0,0 @@
-songs.o: C:/Users/sweshelo/devkitPro/tdm-fumen-manager/source/songs.cpp \
- C:/Users/sweshelo/devkitPro/tdm-fumen-manager/source/include/songs.hpp \
- C:/devkitPro/portlibs/3ds/include/jansson.h \
- C:/devkitPro/portlibs/3ds/include/jansson_config.h
-C:/Users/sweshelo/devkitPro/tdm-fumen-manager/source/include/songs.hpp:
-C:/devkitPro/portlibs/3ds/include/jansson.h:
-C:/devkitPro/portlibs/3ds/include/jansson_config.h:
