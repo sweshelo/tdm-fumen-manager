5cb74df31bbe66bb421c508c2718b39a33a43d14
sweshelo <sweshelo@gmail.com>
2023-01-24 20:03:09 +0900

    [feat] 上書き先の楽曲を選択する画面

--- source/include/gui.hpp
+++ source/include/gui.hpp
@@ -1,5 +1,6 @@
 #include <citro2d.h>
 #include <vector>
+#include <string>
 #include "songs.hpp"
 
 class Gui
@@ -7,15 +8,29 @@ class Gui
   private:
     C2D_TextBuf gBuffer;
     std::vector<C2D_Text> gSongtitleText;
+    std::vector<C2D_Text> guideText;
     std::vector<Song> _songlist;
     std::vector<Song>& songlist = _songlist;
     C3D_RenderTarget* top;
     int cursor = 0;
-    int status = 1;
+    int status = 0;
+    int page = 0;
+    int max_page = 0;
+    std::string songid_download = "";
+    std::string songid_overwrite = "";
     void select_download_song(void);
     void select_overwrite_song(void);
+    std::string navigate[5] = {
+      "ダウンロードする楽曲を選択します",
+      "上書きする楽曲を選択します",
+      "ダウンロードしています",
+      "ダウンロードに成功しました",
+      "ダウンロードに失敗しました",
+    };
 
   public:
+    u32 blue = C2D_Color32f(0.0f,0.0f,1.0f,1.0f);
+    u32 black = C2D_Color32f(0.0f,0.0f,0.0f,1.0f);
     Gui(void);
     void draw(void);
     void set_songlist(std::vector<Song>&);
